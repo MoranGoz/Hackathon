305188988afa8befd3abaff427e4d2dfd5af8524
MoranGoz <morangozlan@hotmail.com>
2018-05-17 16:11:55 +0300

    dd

--- public/events-handler.js
+++ public/events-handler.js
@@ -9,9 +9,29 @@ class EventsHandler {
 
     registerAddPet() {
         $('#addpet').on('click', () => {
+            if ( $("#name").val()===null || 
+            $("#gender").val()===null || 
+            $("#breed").val()===null||
+             $("#color").val()===null|| 
+             $("#size").val()===null || 
+             $("#age").val()===null || 
+             $("#mail").val()===null || 
+             $("#img").val()===null ) {
+                 alert("Please fill out all fields")
+             }
+            else{
             let obj = {name: $("#name").val(), gender: $("#gender").val(),breed: $("#breed").val(),color: $("#color").val(),size: $("#size").val(),age: $("#age").val(),mail: $("#mail").val(),img: $("#img").val(),addpet : "no"};
+            $("#name").val('');
+            $("#gender").val('');
+            $("#breed").val('');
+            $("#color").val('');
+            $("#size").val('');
+            $("#age").val('');
+            $("#mail").val('');
+            $("#img").val('');
             this.petsRepository.addPet(obj).then(()=>{this.petsRenderer.renderPets(this.petsRepository.pets)});
-            })
+            }
+        })
         }
        
 
@@ -45,8 +65,6 @@ class EventsHandler {
                 } 
                 
                 registerAdopt() {
-                    
-
                     $('.pets').on('click','#adopt', () => {
                         console.log('adopted');
                         //function send a mail to the person + tag as adopted
@@ -58,17 +76,16 @@ class EventsHandler {
                 
             // makes the form toggle
             registerToggleForm() {
-               
-
                 $('#addNewPet').on('click', (event) => {
                     $('.pet-form').toggleClass('show');
                     event.preventDefault();
                   });
             }
+
             registerTogglfavorites() {
                 $('.view-favorites').on('click', (event) => {
                     $('.view-favorites').unbind("mouseenter mouseleave");
-                    $('.favorites').toggleClass('hide');
+                    $('.favorites').toggleClass('show');
                     event.preventDefault();
                   });
             }
